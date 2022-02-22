b4ff370f62bd76d98f44ce37920a69130d12765c
Oscar Lastra <56526315+lastra-dev@users.noreply.github.com>
2022-02-21 23:40:57 -0600

    Fix string interpolation

--- migrations/env.py
+++ migrations/env.py
@@ -18,8 +18,11 @@ logger = logging.getLogger('alembic.env')
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from flask import current_app
-config.set_main_option('sqlalchemy.url',
-                       current_app.config.get('SQLALCHEMY_DATABASE_URI'))
+
+# Fix password invalid interpolation error
+db_url_escaped = current_app.config.get('SQLALCHEMY_DATABASE_URI').replace('%', '%%')
+config.set_main_option('sqlalchemy.url', db_url_escaped)
+
 target_metadata = current_app.extensions['migrate'].db.metadata
 
 # other values from the config, defined by the needs of env.py,
